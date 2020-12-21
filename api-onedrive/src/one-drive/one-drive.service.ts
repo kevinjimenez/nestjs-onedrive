3900db1f56b180712749481b9ab228935c73f298
kevinjimenez <kjimenez@29deoctubre.fin.ec>
2020-12-21 16:48:49 -0500

    subida archivos

--- api-onedrive/src/one-drive/one-drive.service.ts
+++ api-onedrive/src/one-drive/one-drive.service.ts
@@ -17,7 +17,7 @@ export class OneDriveService {
       client_id: '1ec0ff85-bb4a-473c-88a6-06f9c5838e75',
       client_secret: 'qQG0lIA_c89cwVWw02UX-j~.z-53kndN0x',
       username: 'rcargua@29deoctubre.fin.ec',
-      password: 'RCskc1996#1',
+      password: 'RCskc1996#2',
       grant_type: 'password',
       resource: 'https://graph.microsoft.com',
     });
@@ -32,12 +32,26 @@ export class OneDriveService {
           },
           data,
         })
-        .subscribe((respuesta: AxiosResponse) => {                 
+        .subscribe((respuesta: AxiosResponse) => {             
+          console.log(respuesta);    
+          onedriveApi.items.createFolder({
+            accessToken: respuesta.data.access_token,
+            drive: 'site', // 'me' | 'user' | 'drive' | 'group' | 'site'
+            driveId: 'coop29deoctubre.sharepoint.com,aa1bfc6f-5923-4a1d-af88-de576567dc68,bf0346cb-9286-4d90-8007-00cafce71bb3', // BLANK | {user_id} | {drive_id} | {group_id} | {sharepoint_site_id}
+            rootItemId: "01R5I4HVJMNFSI7Y4XYVE35KB43KTTFYHQ",
+            name: "ONE-DRIVE-PRUEBA"
+          }).then((item) => {
+          console.log(item)
+          // returns body of https://dev.onedrive.com/items/create.htm#response
+          }).catch(error=>{
+            console.log(error);
+          });
+          
           onedriveApi.items.listChildren({
             accessToken: respuesta.data.access_token,
             itemId: 'root',
-            drive: 'me', // 'me' | 'user' | 'drive' | 'group' | 'site'
-            driveId: '', // BLANK | {user_id} | {drive_id} | {group_id} | {sharepoint_site_id}
+            drive: 'site', // 'me' | 'user' | 'drive' | 'group' | 'site'
+            driveId: 'coop29deoctubre.sharepoint.com,aa1bfc6f-5923-4a1d-af88-de576567dc68,bf0346cb-9286-4d90-8007-00cafce71bb3', // BLANK | {user_id} | {drive_id} | {group_id} | {sharepoint_site_id}
           }).then((childrens) => {
             // list all children of given root directory
             //
@@ -45,7 +59,7 @@ export class OneDriveService {
             // returns body of https://dev.onedrive.com/items/list.htm#response
           }).catch(error=>{
             console.log(error);
-          });         
+          });
         });
     } catch (e) {
       console.log(e);
